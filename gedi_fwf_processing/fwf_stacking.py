730d4ef3525947bae3f661a7ff9212ec1165c8c7
rakass-as <anna-salima.rakass@univ-nantes.fr>
2026-08-05 17:01:06 +0200

    fwf_stacking

---
+++ gedi_fwf_processing/fwf_stacking.py
@@ -0,0 +1,7 @@
+import gedi_fwf_processing.data_prep as gprep
+
+
+# for cells => get gedi shots => extract waveforms from cells shots => 
+# interpolation on zrange rasterisation des fullwaveform agrégées au niveau du pixel
+
+
